1708cd77dd53b2d787a7100b1f54c4dd1a3049ab
Khun Aguero Agnes <157817840+Sen-Benz@users.noreply.github.com>
2025-01-28 23:52:41 +0800

    Add files via upload

---
+++ ccs_screening.sql
@@ -0,0 +1,131 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 28, 2025 at 04:50 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ccs_screening`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `activity_logs`
+--
+-- Error reading structure for table ccs_screening.activity_logs: #1932 - Table &#039;ccs_screening.activity_logs&#039; doesn&#039;t exist in engine
+-- Error reading data for table ccs_screening.activity_logs: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `ccs_screening`.`activity_logs`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+-- Error reading structure for table ccs_screening.admins: #1932 - Table &#039;ccs_screening.admins&#039; doesn&#039;t exist in engine
+-- Error reading data for table ccs_screening.admins: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `ccs_screening`.`admins`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `announcements`
+--
+-- Error reading structure for table ccs_screening.announcements: #1932 - Table &#039;ccs_screening.announcements&#039; doesn&#039;t exist in engine
+-- Error reading data for table ccs_screening.announcements: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `ccs_screening`.`announcements`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `applicants`
+--
+-- Error reading structure for table ccs_screening.applicants: #1932 - Table &#039;ccs_screening.applicants&#039; doesn&#039;t exist in engine
+-- Error reading data for table ccs_screening.applicants: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `ccs_screening`.`applicants`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `applicant_answers`
+--
+-- Error reading structure for table ccs_screening.applicant_answers: #1932 - Table &#039;ccs_screening.applicant_answers&#039; doesn&#039;t exist in engine
+-- Error reading data for table ccs_screening.applicant_answers: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `ccs_screening`.`applicant_answers`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exams`
+--
+-- Error reading structure for table ccs_screening.exams: #1932 - Table &#039;ccs_screening.exams&#039; doesn&#039;t exist in engine
+-- Error reading data for table ccs_screening.exams: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `ccs_screening`.`exams`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exam_results`
+--
+-- Error reading structure for table ccs_screening.exam_results: #1932 - Table &#039;ccs_screening.exam_results&#039; doesn&#039;t exist in engine
+-- Error reading data for table ccs_screening.exam_results: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `ccs_screening`.`exam_results`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `interview_schedules`
+--
+-- Error reading structure for table ccs_screening.interview_schedules: #1932 - Table &#039;ccs_screening.interview_schedules&#039; doesn&#039;t exist in engine
+-- Error reading data for table ccs_screening.interview_schedules: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `ccs_screening`.`interview_schedules`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `interview_scores`
+--
+-- Error reading structure for table ccs_screening.interview_scores: #1932 - Table &#039;ccs_screening.interview_scores&#039; doesn&#039;t exist in engine
+-- Error reading data for table ccs_screening.interview_scores: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `ccs_screening`.`interview_scores`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notifications`
+--
+-- Error reading structure for table ccs_screening.notifications: #1932 - Table &#039;ccs_screening.notifications&#039; doesn&#039;t exist in engine
+-- Error reading data for table ccs_screening.notifications: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `ccs_screening`.`notifications`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
+-- Error reading structure for table ccs_screening.questions: #1932 - Table &#039;ccs_screening.questions&#039; doesn&#039;t exist in engine
+-- Error reading data for table ccs_screening.questions: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `ccs_screening`.`questions`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `super_admins`
+--
+-- Error reading structure for table ccs_screening.super_admins: #1932 - Table &#039;ccs_screening.super_admins&#039; doesn&#039;t exist in engine
+-- Error reading data for table ccs_screening.super_admins: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `ccs_screening`.`super_admins`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table ccs_screening.users: #1932 - Table &#039;ccs_screening.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table ccs_screening.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `ccs_screening`.`users`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
